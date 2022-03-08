f12e0cc68ed3128a928369968d8418e604b8b5bd
Amine Ahdadouch <amine.ahdadouch@epita.fr>
2022-03-08 22:02:41 +0100

    Add test to match 100% coverage
    Add live badges

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # maven_training
+![example workflow](https://github.com/Amine-Ahdadouch/maven_training/actions/workflows/build.yml/badge.svg)
+[![codecov](https://codecov.io/gh/Amine-Ahdadouch/maven_training/branch/main/graph/badge.svg)](https://codecov.io/gh/Amine-Ahdadouch/maven_training)
---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,13 @@
+package fr.lernejo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest extends Sample{
+    @Test
+    public void fact_3_expected_6() {
+        int n = 3;
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.fact(n), 6);
+    }
+}
